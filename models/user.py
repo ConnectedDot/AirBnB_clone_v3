72107c26936f71895ae1280c8f6ed2e2c957ac60
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-02-07 05:49:40 +0100

    Mdel Places

--- models/user.py
+++ models/user.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""
-User Class from Models Module
-"""
-import os
+"""Defines the User class."""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+from sqlalchemy import Column, String
+from sqlalchemy.orm import Relationship
 
 
 class User(BaseModel, Base):
-    """User class handles all application users"""
-    if storage_type == "db":
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+    """Represent a User.
 
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = Relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
+    reviews = Relationship("Review", backref="user",
+                           cascade="all, delete-orphan")
